7e7000c34083b869d8815a426cc66c53125558f9
ollie <ollie@ubuntu.ubuntu-domain>
2012-07-01 19:24:53 +0100

    Added new procewss and motion files plus static pages

---
+++ process.js
@@ -0,0 +1,52 @@
+//File system
+var fs = require("fs");
+
+//Include zeromq
+var zmq = require("zmq-3.0");
+
+//Config
+var config = require("./config.json");
+
+//Read the arguments
+var id = process.argv[2];
+var watermarkPath = process.argv[3];
+var imagePath = process.argv[4];
+
+//Connect to zmq socket
+var socket = zmq.createSocket('push');
+
+//Connect to receive messages
+var address = config.zeromq.protocol + '://' + config.zeromq.host + ':' + config.zeromq.port;
+socket.connect(address);
+
+//Execute the watermark
+var childProcess = require("child_process");
+var command = "composite -gravity SouthWest " + watermarkPath + " " + imagePath + " " + imagePath;
+console.log("Command: ", command);
+childProcess.exec(command,
+function (error, stdout, stderr) {
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if (error !== null) {
+		console.log('exec error: ' + error);
+	}
+
+	//Open file contents
+	var image = fs.readFileSync(imagePath);
+
+	//Broadcast
+	socket.send(id, image);
+
+	//Close
+	socket.close();
+
+	//Delete the original file
+	fs.unlinkSync(imagePath);
+
+});
+
+//Kill self
+setTimeout(function(){
+	process.kill(process.pic, "SIGKILL");
+}, 300);
+
